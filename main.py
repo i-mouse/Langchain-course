6e1ea9374ec56664913293643fb07b35a036c9f9
Imouse <initin.net@gmail.com>
2025-10-20 16:17:16 +0530

    Hello world chain - PromptTemplate | Olamma | OpenAI | Langsmith intro

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import os
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
+# key = os.getenv("OPENAI_API_KEY")
 
 def main():
     print("Hello from langchain-course!\n\n")
+    
+    information = """Sachin Tendulkar, known as the “God of Cricket,” is one of the greatest batsmen in the history of " \
+    "the sport. Born on April 24, 1973, in Mumbai, India, he made his international debut at just 16 years old. Over his " \
+    "24-year career, Sachin set numerous records, including being the first player to score 100 international centuries. " \
+    "His dedication, humility, and passion for the game inspired millions. Beyond statistics, Sachin became a symbol of " \
+    "excellence and perseverance, earning respect both on and off the field."""
 
+    template = "Given the information {information}, I want you to create 1. Short summry 2.two bad points"
+
+    prompt = PromptTemplate(input_variables=["information"], template=template)
+
+    # llm = OpenAI(temperature=0,model="gpt-5")
+    llm = ChatOllama(temperature=0,model="gemma3:270m")
+
+    chain = prompt | llm
+
+    response = chain.invoke({"information" : information})
+    print(response)
 
 if __name__ == "__main__":
     main()
